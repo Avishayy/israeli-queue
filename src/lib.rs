5e495642426e17c3bcc9eba55797878d3474ba41
Avishay Matayev <avikillu@gmail.com>
2018-09-23 09:17:16 -0700

    Slightly less worse tests

--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,14 @@ mod tests {
     fn queue_dequeue() {
         let mut iq: IsraeliQueue<i32> = IsraeliQueue::new();
         iq.queue(15, Some(&test_comparator));
+        iq.queue(1, Some(&test_comparator));
+        iq.queue(13, Some(&test_comparator));
+        iq.queue(15, Some(&test_comparator));
         assert!(!iq.is_empty());
         assert_eq!(15, *iq.peek().unwrap());
         assert_eq!(15, iq.dequeue().unwrap());
+        assert_eq!(15, iq.dequeue().unwrap());
+        assert_eq!(1, iq.dequeue().unwrap());
+        assert_eq!(13, iq.dequeue().unwrap());
     }
 }
